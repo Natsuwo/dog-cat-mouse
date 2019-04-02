b860c0a97c70971e65d9591748f01b19f6c214f9
kirigayakazuto42@gmail.com <kirigayakazuto42@gmail.com>
2019-04-02 11:02:57 +0700

    Unline break

--- index.js
+++ index.js
@@ -52,6 +52,9 @@
  * 2/ git push origin <branch>
  * 3/ create a pull request on Github
  * 4/ review code
+ *     4.1. review code online (github)
+ *     4.2. fetch branch into local to test offline (optional)
+ *     4.3. approve the pull request
  * 5/ merge to master
  */
 
